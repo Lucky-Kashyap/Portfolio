2595d30f9ec2a57e25e50d48db41000a2ae5801c
Lucky-Kashyap <luckykashyap3430@gmail.com>
2026-08-21 18:17:37 +0530

    Remove output configuration from Next.js config file

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ import { fileURLToPath } from "url";
 const root = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
